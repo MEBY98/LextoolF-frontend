ea780fea839c2dda40843ee0f010de1a24fec086
MEBY98 <mebordony@gmail.com>
2021-10-13 08:42:21 -0400

    Capture Screen Shot Component

---
+++ src/components/pages/entry/VueCroppie/CaptureScreenShot.vue
@@ -0,0 +1,12 @@
+<template></template>
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+import UseAddPasteEvent from './UseAddPasteEvent';
+export default defineComponent({
+  emits: ['paste'],
+  setup(props, context) {
+    const { addPasteEvent } = UseAddPasteEvent(context);
+    onMounted(addPasteEvent);
+  },
+});
+</script>
