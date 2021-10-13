9fb58881065278aeb47dd7d26ed99cbd53accf90
MEBY98 <mebordony@gmail.com>
2021-10-13 08:03:21 -0400

    Adjust Ubication graphql services

--- src/graphql/modules/ubication/model.ts
+++ src/graphql/modules/ubication/model.ts
@@ -1,6 +1,7 @@
 import { apolloQuery, apolloMutate } from '@/graphql/apollo';
 import { FetchPolicy, apolloClient } from '@/graphql/apolloProvider';
 import { getAllUbicationsQuery } from './querys';
+import { CreateUbicationMutation, EditUbicationMutation } from './mutations';
 
 export class Ubication {
   static getAllUbications() {
@@ -10,5 +11,13 @@ export class Ubication {
       FetchPolicy.network_only,
       apolloClient
     );
+  }
+
+  static createUbication(newUbication) {
+    return apolloMutate(CreateUbicationMutation, { newUbication }, null, null);
+  }
+
+  static editUbication(editedUbication) {
+    return apolloMutate(EditUbicationMutation, { editedUbication }, null, null);
   }
 }
